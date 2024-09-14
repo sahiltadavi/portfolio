12f06dd322c609fe572f276dca45712ffbc566cc
sahiltadavi <tadavi363@gmail.com>
2024-09-14 20:15:22 +0530

    update project file

--- src/examples/Project.js
+++ src/examples/Project.js
@@ -2,28 +2,25 @@ import React from 'react';
 import "./Project.css";
 import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.jpg";
+
 const Project = () => {
     return (
-        <body>
-
-            <div className='bodydiv'>
-                <h3>Projects</h3>
-                <div className='first1'>
-                    <div className='insideFirst1'>
-                        <img src={project2} className='projectImg' alt="description of the image"></img>
-                    </div>
-                    <h1 className='proH1'>smart maintainance portal</h1>
+        <div className='bodydiv'>
+            <h3>Projects</h3>
+            <div className='first1'>
+                <div className='insideFirst1'>
+                    <img src={project2} className='projectImg' alt="Smart Maintenance Portal screenshot"></img>
                 </div>
+                <h1 className='proH1'>Smart Maintenance Portal</h1>
+            </div>
 
-                <div className='Pro_second'>
-                    <h1 className='proH12'>plant leaf disease prediction using machine learning </h1>
-                    <div>
-                        <img src={project1} className='projectImg1' alt="description of the image"></img>
-                    </div>
+            <div className='Pro_second'>
+                <h1 className='proH12'>Plant Leaf Disease Prediction Using Machine Learning</h1>
+                <div>
+                    <img src={project1} className='projectImg1' alt="Plant leaf disease prediction app screenshot"></img>
                 </div>
             </div>
-        </body>
-
+        </div>
     )
 }
 
